9e923e176ccc091a5fdc6f3453ff8ceccedf04c5
Gonzalo Guevara <gonzarock@yahoo.com>
2017-03-20 17:25:15 -0300

    Completado todo

--- js/2_todo.js
+++ js/2_todo.js
@@ -1,24 +1,51 @@
 new Vue({
     el: '#todo',
     data: {
-        todos: [
-            { completado: false, title: 'Comprar pan'},
-            { completado: false, title: 'Aprender vue'},
-            { completado: false, title: 'Salir a correr'}
+        todos: [{
+                completado: false,
+                title: 'Comprar pan'
+            },
+            {
+                completado: false,
+                title: 'Aprender vue'
+            },
+            {
+                completado: false,
+                title: 'Salir a correr'
+            }
         ],
         nuevoTodo: ''
     },
     computed: {
-        calcularTotal: function(){
-            return this.todos.filter(function(todo){
+        calcularTotal: function () {
+            console.log('Calcular total');
+            return this.todos.filter(function (todo) {
                 return todo.completado == true;
             }).length;
+        },
+        todosCompletados: function () {
+            console.log('Todos completados');
+            return this.todos.filter(function (todo) {
+                return todo.completado == true;
+            });
+        },
+        todosPorCompletar: function () {
+            console.log('Por completar');
+            return this.todos.filter(function (todo) {
+                return todo.completado == false;
+            });
         }
     },
     methods: {
-        agregaTodo: function(){
-            this.todos.unshift({title: this.nuevoTodo, completado: false});
+        agregaTodo: function () {
+            this.todos.unshift({
+                title: this.nuevoTodo,
+                completado: false
+            });
             this.nuevoTodo = '';
+        },
+        eliminarTodo: function(index){
+            this.todos.splice(index, 1);
         }
     }
 });
